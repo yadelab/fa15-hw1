2f26ae77c1510f3e259dceef55443c9ab46066f3
unknown <yabraham@D-Lab03.sscl.berkeley.edu>
2015-09-18 23:27:49 -0700

    homework 1

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,12 @@
 class Foobar
   # Q4 CODE HERE
+  def self.baz(arr)
+  	str = arr.map(&:to_i)
+  	c =   arr.map{|a| a+2}
+  	evn = c.select{|a| a%2 == 0}
+  	nodup = evn.uniq
+  	ten = nodup.select{|a| a<10}
+  	sum = ten.inject(:+)
+  	sum
+
 end
--- hw1.rb
+++ hw1.rb
@@ -1,13 +1,21 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  y = a + b
+  y = y**2
+  y
+  
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
+  arr = a.sort 
+  arr.map{|a| a+1}
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
+  concat = first_name + " "+ last_name
+  concat
 end
 
 def blockin_time(a)
